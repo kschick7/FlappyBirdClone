a43d15fe4f7bd953e561db72c6082b7c079d5f44
kschick7 <kschick@umich.edu>
2015-11-25 04:20:26 -0500

    Obtained original Flappy Bird assets, intend to reskin later

--- core/src/com/kyleschick/hoverboy/HoverBoy.java
+++ core/src/com/kyleschick/hoverboy/HoverBoy.java
@@ -8,12 +8,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class HoverBoy extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture backgroundTexture;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		backgroundTexture = new Texture("background.png");
 	}
 
 	@Override
@@ -21,7 +21,7 @@ public class HoverBoy extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(backgroundTexture, 0, 0);
 		batch.end();
 	}
 }
