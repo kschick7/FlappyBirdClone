6562f923d0b3291732b2f8baf5529be59ef47b10
kschick7 <kschick@umich.edu>
2015-11-25 17:18:38 -0500

    Implemented game states

--- core/src/com/kyleschick/hoverboy/HoverBoy.java
+++ core/src/com/kyleschick/hoverboy/HoverBoy.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class HoverBoy extends ApplicationAdapter {
 	private static final double GRAVITY = -0.2;
 	private static final double JUMP_SPEED = 6;
+	public enum State { MENU, INGAME, GAMEOVER, PAUSED }
 
 	SpriteBatch batch;
 	Texture backgroundTexture;
@@ -17,7 +18,8 @@ public class HoverBoy extends ApplicationAdapter {
 	Bird bird;
 	Clock clock;
 	Boolean spaceDown;
-	
+	State state;
+
 	@Override
 	public void create () {
 		bird = new Bird(80, 300);
@@ -28,6 +30,7 @@ public class HoverBoy extends ApplicationAdapter {
 		birdTextures = new Texture[2];
 		birdTextures[0] = new Texture("bird0.png");
 		birdTextures[1] = new Texture("bird1.png");
+		state = State.MENU;
 	}
 
 	@Override
@@ -36,17 +39,28 @@ public class HoverBoy extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(backgroundTexture, 0, 0);
-		batch.draw(birdTextures[0], bird.getX(), bird.getY());
+
+		if (state == State.INGAME) {
+			batch.draw(backgroundTexture, 0, 0);
+			batch.draw(birdTextures[0], bird.getX(), bird.getY());
+
+			if (!spaceDown && Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+				bird.setVerticleSpeed(JUMP_SPEED);
+				spaceDown = true;
+			} else {
+				bird.changeVerticleSpeed(GRAVITY);
+				spaceDown = Gdx.input.isKeyPressed(Input.Keys.SPACE);
+			}
+			bird.move();
+		} else if (state == State.MENU) {
+			if (Gdx.input.isKeyPressed(Input.Keys.ENTER))
+				state = State.INGAME;
+		}
+
+
 		batch.end();
 
-		if(!spaceDown && Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-			bird.setVerticleSpeed(JUMP_SPEED);
-			spaceDown = true;
-		} else {
-			bird.changeVerticleSpeed(GRAVITY);
-			spaceDown = Gdx.input.isKeyPressed(Input.Keys.SPACE);
-		}
-		bird.move();
+
+
 	}
 }
