c130e0244fa64a2f181570c167c4ef5f9ae8953c
kschick7 <kschick@umich.edu>
2015-11-27 02:33:42 -0500

    Added collision and game over state

--- core/src/com/kyleschick/hoverboy/HoverBoy.java
+++ core/src/com/kyleschick/hoverboy/HoverBoy.java
@@ -15,10 +15,12 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class HoverBoy extends ApplicationAdapter {
-	private static final float GRAVITY = -18;
-	private static final float JUMP_SPEED = 400;
+	private static final float GRAVITY = -25;
+	private static final float JUMP_SPEED = 500;
 	private static final float OBSTACLE_INTERVAL = 1500;
 	private static final float HORIZONTAL_SPEED = 175;
+	private static final int BIRD_START_Y = 300;
+	private static final int BIRD_START_X = 70;
 
 	public enum State { MENU, INGAME, GAMEOVER, PAUSED }
 
@@ -36,7 +38,7 @@ public class HoverBoy extends ApplicationAdapter {
 
 	@Override
 	public void create () {
-		bird = new Rectangle(70, 300, 40, 30);
+		bird = new Rectangle(BIRD_START_X, BIRD_START_Y, 40, 30);
 		obstacleTime = 0;
 		batch = new SpriteBatch();
 		obstaclesTop = new ArrayList();
@@ -60,33 +62,18 @@ public class HoverBoy extends ApplicationAdapter {
 
 		if (state == State.INGAME) {
 			batch.draw(backgroundTexture, 0, 0);
-
 			if (inputManager.isKeyPressed(Input.Keys.SPACE))
 				birdSpeed = JUMP_SPEED;
-			else
-				birdSpeed += GRAVITY;
-
-			move(bird, 0, birdSpeed);
-			if (birdSpeed < 0)
-				batch.draw(birdTextures[0], bird.getX(), bird.getY());
-			else
-				batch.draw(birdTextures[1], bird.getX(), bird.getY());
-
-			if (inputManager.isKeyPressed(Input.Keys.ENTER))
-				state = State.PAUSED;
+			renderObstacles(true);
+			renderBird();
 
 			if (System.currentTimeMillis() - obstacleTime >= OBSTACLE_INTERVAL) {
 				obstacleTime = System.currentTimeMillis();
 				spawnObstacles();
 			}
 
-			for (int i = 0; i < obstaclesTop.size(); ++i) {
-				move(obstaclesTop.get(i), -HORIZONTAL_SPEED, 0);
-				move(obstaclesBottom.get(i), -HORIZONTAL_SPEED, 0);
-				batch.draw(pipeTextures[0], obstaclesTop.get(i).x, obstaclesTop.get(i).y);
-				batch.draw(pipeTextures[1], obstaclesBottom.get(i).x, obstaclesBottom.get(i).y);
-			}
-
+			if (inputManager.isKeyPressed(Input.Keys.ENTER))
+				state = State.PAUSED;
 		} else if (state == State.MENU) {
 			if (inputManager.isKeyPressed(Input.Keys.ENTER)) {
 				state = State.INGAME;
@@ -95,6 +82,17 @@ public class HoverBoy extends ApplicationAdapter {
 		} else if (state == State.PAUSED) {
 			if (inputManager.isKeyPressed(Input.Keys.ENTER))
 				state = State.INGAME;
+		} else if (state == State.GAMEOVER) {
+			batch.draw(backgroundTexture, 0, 0);
+			renderObstacles(false);
+			renderBird();
+			if (inputManager.isKeyPressed(Input.Keys.ENTER)) {
+				state = State.INGAME;
+				obstaclesTop.clear();
+				obstaclesBottom.clear();
+				bird.setY(BIRD_START_Y);
+				birdSpeed = 0;
+			}
 		}
 
 		batch.end();
@@ -109,5 +107,28 @@ public class HoverBoy extends ApplicationAdapter {
 		int openingHeight = MathUtils.random(100, 450) ;
 		obstaclesTop.add(new Rectangle(600, 640 - openingHeight, 100, 500));
 		obstaclesBottom.add(new Rectangle(600, -openingHeight, 100, 500));
+	}
+
+	public void renderObstacles(boolean moving) {
+		for (int i = 0; i < obstaclesTop.size(); ++i) {
+			if (moving) {
+				move(obstaclesTop.get(i), -HORIZONTAL_SPEED, 0);
+				move(obstaclesBottom.get(i), -HORIZONTAL_SPEED, 0);
+			}
+			batch.draw(pipeTextures[0], obstaclesTop.get(i).x, obstaclesTop.get(i).y);
+			batch.draw(pipeTextures[1], obstaclesBottom.get(i).x, obstaclesBottom.get(i).y);
+			if ((bird.overlaps(obstaclesTop.get(i)) || bird.overlaps(obstaclesBottom.get(i)))
+					&& state == State.INGAME)
+				state = State.GAMEOVER;
+		}
+	}
+
+	public void renderBird() {
+		birdSpeed += GRAVITY;
+		move(bird, 0, birdSpeed);
+		if (birdSpeed < 0)
+			batch.draw(birdTextures[0], bird.getX(), bird.getY());
+		else
+			batch.draw(birdTextures[1], bird.getX(), bird.getY());
 	}
 }
